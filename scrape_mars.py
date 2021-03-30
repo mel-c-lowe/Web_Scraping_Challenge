1185bc8f8ee85fb0b8b08f6ff23055f6ac6f61d7
Melissa Lowe <mel.c.lowe@gmail.com>
2021-03-29 21:04:28 -0500

    Started mars_scrape function

---
+++ scrape_mars.py
@@ -0,0 +1,29 @@
+# Dependencies
+from bs4 import BeautifulSoup as BS
+from splinter import Browser
+from webdriver_manager.chrome import ChromeDriverManager
+
+def scrape():
+
+    # Initialize browser
+
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
+
+    mars_data = {}
+
+    # Portion of function for news title and teaser copy
+
+    news_url = ("https://redplanetscience.com/#")
+    browser.visit(news_url)
+    html = browser.html
+    soup = BS(html, 'html.parser')
+
+    # Retrieve the title and news teaser
+    news_title = soup.find('div', class_='content_title').text
+    article_teaser = soup.find('div', class_='article_teaser_body').text
+
+    mars_data["news_title"] = news_title
+    mars_data["article_teaser"] = article_teaser
+
+    return mars_data
