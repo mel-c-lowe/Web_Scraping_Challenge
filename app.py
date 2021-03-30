1793790ba55b9561082ac540ca172b16a8e67ae4
Melissa Lowe <mel.c.lowe@gmail.com>
2021-03-29 21:38:59 -0500

    Well, something is happening, but not much. But not nothing

--- app.py
+++ app.py
@@ -6,10 +6,27 @@ import scrape_mars
 # Create flask app
 app = Flask(__name__)
 
+# Use PyMongo to establish Mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/app")
+
 # Route at index
 @app.route("/")
 def index():
-    return "Hello, Mars!"
+    return "Hello, Mars! Let's get data!"
+
+# Testing initial news scrape
+@app.route("/scrape")
+def scrape_news():
+
+    # Run scrape function
+
+    mars_db = mongo.db.mars_db
+    mars_data = scrape_mars.scrape_news()
+
+
+    mars_db.update({}, mars_data, upsert=True)
+    
+    return redirect("/", code=302)
 
 
 
